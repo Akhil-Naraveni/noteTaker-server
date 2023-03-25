3f8ff7087444318134e694aea3427b3c7ce276d9
Naraveni Akhil <naraveniakhil@email.com>
2023-03-25 22:42:06 +0530

    commmit

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+const URL = "mongodb+srv://naraveniakhil:Aakhil2244@instacloneserver.tqjlz9f.mongodb.net/?retryWrites=true&w=majority";
 
 async function connection() {
-    console.log("connected to mongoDB")
-    await mongoose.connect("mongodb://localhost/notetaker")
+    await mongoose.connect(URL,{useNewUrlParser: true, useUnifiedTopology:true})
+        console.log("connected to mongoDB")
+
 }
 
 module.exports = connection;
